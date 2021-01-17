6b2e10ded2af97354eb9152c89749fe48804758d
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-01-17 18:17:29 +0400

    update

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBe_l8WQqHogWbpZGqySJtF-EeoOFfklEs",
+    authDomain: "clone-9510c.firebaseapp.com",
+    projectId: "clone-9510c",
+    storageBucket: "clone-9510c.appspot.com",
+    messagingSenderId: "425494605204",
+    appId: "1:425494605204:web:37fd7708f5fd0510967381"
+  };
